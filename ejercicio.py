91d38586df1e38435f0130203fd35e59d6bafdef
Nico <144184381+SataNico04@users.noreply.github.com>
2024-10-21 11:42:26 +0200

    Update ejercicio.py

--- ejercicio.py
+++ ejercicio.py
@@ -1,6 +1,8 @@
 # Escribir un programa que almacene en una lista los siguientes 
 # precios: 50, 75, 46, 22, 80, 65, 8 y muestre por pantalla el menor y el mayor de los precios.
 
+# Ejercicio hecho por Nicolás Ruiz
+
 def mayor(lista):
     numero=0
     for i in lista:
